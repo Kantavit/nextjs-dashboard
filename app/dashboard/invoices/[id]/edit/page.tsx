94ae94c8040a2d76cb307c03f0fc50e0edce740d
kantavit <krite-18354@hotmail.com>
2024-02-15 10:45:49 +0700

    add meta title & delete invoices

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,6 +4,10 @@ import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Edit',
+};
+
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
@@ -31,7 +35,3 @@ export default async function Page({ params }: { params: { id: string } }) {
     </main>
   );
 }
-
-export const metadata: Metadata = {
-  title: 'Invoices | Acme Dashboard',
-};
